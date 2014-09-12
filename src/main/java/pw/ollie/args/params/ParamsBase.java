946dfd98907269940bd4a8c146fb419283c495e4
DziNeIT <o.dzineit@gmail.com>
2014-09-12 20:29:01 +0100

    Make FlagInfo a private inner class of ParamsBase to circumvent confusion

--- src/main/java/pw/ollie/args/params/ParamsBase.java
+++ src/main/java/pw/ollie/args/params/ParamsBase.java
@@ -164,7 +164,7 @@ public class ParamsBase {
         int before = 0;
         boolean passedCommand = false;
         int amtRequired = 0;
-        List<FlagInfo> flags = new ArrayList<FlagInfo>();
+        List<FlagInfo> flags = new ArrayList<>();
 
         final char[] characters = usageString.toCharArray();
         for (int i = 0; i < characters.length; i++) {
@@ -212,9 +212,8 @@ public class ParamsBase {
 
                         desc.append(toAppend);
                     }
-                    flags.add(
-                            new FlagInfo(String.valueOf(next), desc.toString(),
-                                    isOptional));
+                    flags.add(new FlagInfo(String.valueOf(next),
+                            desc.toString(), isOptional));
 
                     i += 2;
                     required = false;
@@ -238,5 +237,43 @@ public class ParamsBase {
         }
 
         return new ParamsBase(res, before, amtRequired, flags);
+    }
+
+    /**
+     * Basic flag information, used only in {@link ParamsBase}.
+     */
+    private static class FlagInfo {
+        /**
+         * Name (denotation) of the flag. This represents the first 'component' of a
+         * flag. For example, in '-f val', this would be 'f'.
+         */
+        private final String name;
+        /**
+         * A short description of the flag. Should be one word. This represents the
+         * second 'component' of a flag. In '-f val', this would be 'val'.
+         */
+        private final String desc;
+        /**
+         * Whether this flag is optional.
+         */
+        private final boolean optional;
+
+        public FlagInfo(String name, String desc, boolean optional) {
+            this.name = name;
+            this.desc = desc;
+            this.optional = optional;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getDesc() {
+            return desc;
+        }
+
+        public boolean isOptional() {
+            return optional;
+        }
     }
 }
